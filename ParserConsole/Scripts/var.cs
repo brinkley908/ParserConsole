3086a5ee5a8faa3acb9cc780d1d3a3c5f55c9243
RJBHOMEW11\brink <brinkley908@gmail.com>
2022-10-23 18:43:49 +0100

    script change

--- ParserConsole/Scripts/var.cs
+++ ParserConsole/Scripts/var.cs
@@ -1,12 +1,19 @@
-﻿
-main()
+﻿main()
 {
     var d = true;
-    int x = 0;
+    int x = -1;
 
+    datetime dat1 = Date(2019, 05, 09, 14, 30, 12);
+    datetime dat2 = Now();
+
+    printf("Date 1=", dat1);
+    printf("Date 2=", dat2);
+
+    var diff = DateDiff(dat1, dat2, 3).Seconds();
+    printf("Diff ", DateDiff(dat1, dat2, 3).TotalSeconds());
 
     json i = { "contacts": [{ "name": "john" }, { "name": "jane" }]};
 
-printf(
+
 
 }
