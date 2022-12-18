f6c93414e2720e77fe7634ea18d9b209ad142ab7
RJBHOMEW11\brink <brinkley908@gmail.com>
2022-12-18 13:59:14 +0000

    SECURITY

--- ParserConsole/Program.cs
+++ ParserConsole/Program.cs
@@ -1,9 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 using Parser;
 using Parser.Service;
 using ParserConsole.Service;
+using Security.Models.ASpNet;
+using System.Text;
 
 var services = new ServiceCollection();
 
@@ -11,17 +16,53 @@ services.AddScoped<IParserService, ParserService>();
 services.AddScoped<cParserDemo>();
 services.AddScoped<IFileIOService, FileIOService>();
 
+services.AddIdentity<IdentityUser, IdentityRole>(
+        options =>
+        {
+            options.SignIn.RequireConfirmedAccount = false;
+
+            //Other options go here
+        }
+        )
+    .AddEntityFrameworkStores<IdentityContext>()
+    .AddDefaultTokenProviders();
+
+services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        { 
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = Jwt.Issuer,
+            ValidAudience = Jwt.Issuer,
+            IssuerSigningKey = new
+            SymmetricSecurityKey
+            (Encoding.UTF8.GetBytes
+            (Jwt.Key))
+        };
+    });
+
 using ServiceProvider serviceProvider = services.BuildServiceProvider();
+
 var cParser = serviceProvider.GetRequiredService<cParserDemo>();
 
 while (true)
 {
     Console.WriteLine("\r\nEnter File Name (exit, quit or q to quit)");
     var file = Console.ReadLine();
-    
-    if(file.ToLower()=="exit" || file.ToLower() == "quit" || file.ToLower() == "q") break;
 
     if (string.IsNullOrWhiteSpace(file)) file = "test";
+
+    if (file.ToLower()=="exit" || file.ToLower() == "quit" || file.ToLower() == "q") break;
+
 
     cParser.FileName = $"E:\\Dev\\ParserConsole\\ParserConsole\\Scripts\\{file}.cs";
     //cParser.Compile();
--- ParserConsole/Scripts/var.cs
+++ ParserConsole/Scripts/var.cs
@@ -6,11 +6,14 @@
     datetime dat1 = Date(2019, 05, 09, 14, 30, 12);
     datetime dat2 = Now();
 
-    printf("Date 1=", dat1);
+    var s = "abc  ";
+    printf(s.Trim(), ":");
+
+    printf("Date 1=", dat1.ToString());
     printf("Date 2=", dat2);
 
     var diff = DateDiff(dat1, dat2, 3).Seconds();
-    printf("Diff ", DateDiff(dat1, dat2, 3).TotalSeconds());
+    printf("Diff ", DateDiff(dat2, dat1, 3).Days());
 
     json i = { "contacts": [{ "name": "john" }, { "name": "jane" }]};
 
